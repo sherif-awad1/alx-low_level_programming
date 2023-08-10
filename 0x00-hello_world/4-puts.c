6453dcc1c875a8261ecf7e5b4001acb5b13ca535
Sherif Awad <sherif.hero1@gmail.com>
2023-08-10 04:40:46 -0700

    first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 /*libary*/
 #include <stdio.h>
 
-/**
- * main - entry point
- * return: always 0
- */
+/* main - entry point */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/* retur - Awlays 0 (no return) */
 	return (0);
 }
