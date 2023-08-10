de38c2ef842a3c0b2fec62bbedf30b9c5c5d126c
Sherif Awad <sherif.hero1@gmail.com>
2023-08-10 09:14:00 -0700

    assam

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
