fe81a01c0e13c9a83eede4ea3eaca79ab8a7960e
Sherif Awad <sherif.hero1@gmail.com>
2023-08-11 03:32:53 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph < 'z' ; alph++)
+	{
+		putchar(alph);
+	}
+
+	return (0);
+}
