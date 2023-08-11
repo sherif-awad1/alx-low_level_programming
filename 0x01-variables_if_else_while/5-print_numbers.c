3e25e4bddaa2729b701964dfa9e25aa88d204ddb
Sherif Awad <sherif.hero1@gmail.com>
2023-08-11 04:21:46 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Alwaus 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		printf("%i", num);
+	}
+	printf("\n");
+	return (0);
+}
