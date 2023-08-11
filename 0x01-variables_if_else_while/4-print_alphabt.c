10c42168815087d2215cc1af99af2ee780f4fd43
Sherif Awad <sherif.hero1@gmail.com>
2023-08-11 04:09:40 -0700

    exept e q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,12 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a' ; alpha <= 'z' && alpha != 'q' && alpha != 'e' ; alpha++)
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 	{
+		if (alpha != 'e' && alpha != 'q')
+		{
 		putchar(alpha);
+		}
 	}
 	putchar('\n');
 	return (0);
