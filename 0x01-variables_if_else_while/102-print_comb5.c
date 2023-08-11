ef6b7306007a81504dd017088a8b66f91e5b4942
Sherif Awad <sherif.hero1@gmail.com>
2023-08-11 09:27:51 -0700

    error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
  *Return: Always 0 (Success)
  */
 
@@ -9,26 +9,34 @@ int main(void)
 {
 	int one;
 	int ten;
+	int onee;
+	int tenn;
 
-	for (one = 0; one <= 99; one++)
+	for (one = '0'; one <= '9'; one++)
 	{
-		for (ten = 0; ten <= 99; ten++)
+		for (ten = '0'; ten <= '9'; ten++)
 		{
-			if (one < ten && one != ten)
+			for (onee = one; onee <= '9'; onee++)
 			{
-				putchar((one / 10) + '0');
-				putchar((one % 10) + '0');
-				putchar(' ');
-				putchar((ten / 10) + '0');
-				putchar((ten % 10) + '0');
-				if (one != 98 && ten != 99)
+				for (tenn = ten + 1; tenn <= '9'; tenn++)
 				{
-					putchar(',');
+					putchar(one);
+					putchar(ten);
 					putchar(' ');
+					putchar(onee);
+					putchar(tenn);
+
+					if (!((one == '9' && ten == '8') && (onee == '9' && tenn == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				tenn = '0';
 			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
