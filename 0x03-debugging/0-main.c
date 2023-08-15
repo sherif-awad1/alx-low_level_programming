647782c5b0c7599da84c170e2f81881545834bc8
Sherif Awad <sherif.hero1@gmail.com>
2023-08-15 16:51:38 +0300

    postive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+
+	return (0);
+
