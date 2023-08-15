5c037f4dc848598d5f47562bf2b02d28aaf60d17
Sherif Awad <sherif.hero1@gmail.com>
2023-08-15 15:23:55 +0300

    last

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 	int alpha;
 	char alpha1;
 
-	for (alpha = 0;alpha < 10; alpha++)
+	for (alpha = 0; alpha < 10; alpha++)
 	{
-		for (alpha1 = 'a';alpha1 <= 'z';alpha1++)
+		for (alpha1 = 'a'; alpha1 <= 'z'; alpha1++)
 		{
 			_putchar(alpha1);
 		}
