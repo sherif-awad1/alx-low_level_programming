56b1fe57dbaadf032aeba9de337ce4b19b06c476
Sherif Awad <sherif.hero1@gmail.com>
2023-08-15 12:27:51 +0300

    ne line

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,11 +10,11 @@ int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || c >= ('A' && c <= 'Z'))
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		return(0);
+		return(1);
 	}
 	_putchar('\n');
 }
