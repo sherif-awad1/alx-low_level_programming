ea358d34c1b9541f2b48262b7a1cc2d1f6a4adc9
Sherif Awad <sherif.hero1@gmail.com>
2023-08-15 15:33:58 +0300

    last

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
-/*
- * jack_burn - will count every minit in the next 24 h
- * return alway 0
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 
 void jack_bauer(void)
