aa65123b06e45db606b794cdb0e24b18820ad2b2
Sherif Awad <sherif.hero1@gmail.com>
2023-08-15 16:23:52 +0300

    98

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
 #include "main.h"
-/*print_times_table - print any tabel greater than 15 less than 0
- * n will chack
- * return will be 0
+
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
 
 void print_times_table(int n)
 {
-	int row, col, tot;
-	
+	int row;
+	int column;
+	int product;
+
 	if (n >= 0 && n <= 15)
 	{
-		for (row = '0'; row <= n; row++)
+		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <= n; col++)
+			for (column = 0; column <= n; column++)
 			{
-				tot = (row * col);
-				if (col == 0)
-				{
-					_putchar('0' + tot);
-				}
-				else if (tot < 10 && col != 0)
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(tot + '0');
-				}
-				else if ( tot >= 10  && tot < 100)
+					if (product <= 9)
 					{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (tot / 10));
-					_putchar('0' + (tot % 10));
-				}
-				else if (tot >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((tot / 100) + '0');
-					_putchar(((tot / 10) % 10) + '0');
-					_putchar((tot % 10) + '0');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
 				}
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
