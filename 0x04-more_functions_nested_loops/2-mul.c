9689add5ae136a92513d781e80650f15eb9ad943
Sherif Awad <sherif.hero1@gmail.com>
2023-08-17 13:07:16 +0300

    multiplues

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow integers
+ * @a: to check the first integer
+ * @b: to check the scanod integer
+ * Return: mult
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = (a * b);
+
+	return (mult);
+}
