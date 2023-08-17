d79c26d5e63893b134f976d6ae892c79c5036037
Sherif Awad <sherif.hero1@gmail.com>
2023-08-17 18:29:20 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,10 +15,10 @@ int main(void)
 	while (a <= 100)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
-			printf("FizzBuzz");
-		else if (a % 3 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0 && a % 5 != 0)
 			printf("Fizz ");
-		else if (a % 5 == 0)
+		else if (a % 5 == 0 && a % 3 != 0)
 			printf("Buzz ");
 		else
 			printf("%d ", a);
