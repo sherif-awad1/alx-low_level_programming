c53689dda7b15358cbe288465d117ef2c8c98360
Sherif Awad <sherif.hero1@gmail.com>
2023-08-17 19:53:51 +0300

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/**
+ * main - find the prime factors
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	long int a = 612852475143;
+	int b;
+
+	while (a % 2 == 0)
+	{
+		printf("%d ", 2);
+		a = a / 2;
+	}
+
+	for (b = 3; b * b <= a; b = b + 2)
+	{
+		while (a % b == 0)
+		{
+			printf("%d ", b);
+			a = a / b;
+		}
+	}
+	if (a > 2)
+		printf("%ld\n", a);
+	return (0);
+}
