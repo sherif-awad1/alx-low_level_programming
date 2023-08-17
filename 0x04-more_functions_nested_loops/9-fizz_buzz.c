66e72b1568b1c628b0847d48664a4f18b0f42911
Sherif Awad <sherif.hero1@gmail.com>
2023-08-17 18:22:30 +0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - print while multiples of 3 and 5 and 15
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 1;
+
+	while (a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+		a++;
+	}
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+#include <stdio.h>
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
