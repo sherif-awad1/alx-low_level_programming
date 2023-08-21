101ee1319e18de7f1453999113be9a1a39256ffb
Sherif Awad <sherif.hero1@gmail.com>
2023-08-21 23:52:37 +0300

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point to generates random vaild password
+i * 
+ * Return: Always 0
+ */
+
+void main()
+{
+	char p[100];
+	int  i;
+	char alpha[] = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		p[i] = alpha[rand() % strlen(alpha)];
+		printf("%c", p[i]);
+	}
+	return (0);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef main_h
 #define main_h
 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
