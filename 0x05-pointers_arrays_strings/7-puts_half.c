e2923ecd209f8a0b6e6fd1e92b53e8d6d63a2f32
Sherif Awad <sherif.hero1@gmail.com>
2023-08-21 20:04:52 +0300

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: to check
+ */
+
+void puts_half(char *str)
+{
+	int leng, b, a;
+
+	leng = 0;
+
+	while (str[leng] < '\0')
+	{
+		leng++;
+	}
+
+	if (leng % 2 == 0)
+	{
+		for (b = leng / 2; str[b] != '\0'; b++)
+		{
+			_putchar(str[b]);
+		}
+	}
+	else if (leng % 2 == 1)
+	{
+		for (a = (leng - 1) / 2; a < leng - 1; a++)
+		{
+			_putchar(str[a + 1]);
+		}
+	}
+	_putchar('\n');
+}
