909f365f15d74e64303b1dfff41999e6924b5e66
Sherif Awad <sherif.hero1@gmail.com>
2023-08-21 20:25:08 +0300

    arrat

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: to check
+ * @n: is the number of array elements
+ */
+
+void print_array(int *a, int n)
+{
+	int s;
+
+	for (s = 0; s < n; s++)
+	{
+		if (a == 0)
+		{
+			printf("%d", a[s]);
+		}
+		else
+			printf(", %d", a[s]);
+	}
+}
