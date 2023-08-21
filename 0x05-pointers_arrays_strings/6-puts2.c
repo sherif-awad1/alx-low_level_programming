1a5b5ec06814472e9082aba14974eeb2d995ecb8
Sherif Awad <sherif.hero1@gmail.com>
2023-08-21 19:21:55 +0300

    other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of stinge
+ * @str: to check
+ */
+
+void puts2(char *str)
+{
+	int leng, a;
+
+	while (str[leng] != '\0')
+	{
+		leng++;
+	}
+
+	for (a = 0; a <= leng; a + 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
