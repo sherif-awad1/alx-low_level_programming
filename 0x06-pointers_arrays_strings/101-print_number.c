c240feb7767bf1504b37ce881a34be794a396e75
Sherif Awad <sherif.hero1@gmail.com>
2023-08-24 00:10:56 +0300

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,6 +9,7 @@ void print_number(int n)
 {
 	unsigned int i;
 
+	i = n;
 
 	if (n < 0)
 	{
@@ -20,7 +21,7 @@ void print_number(int n)
 	{
 		i = n;
 	}
-	if (i / 10)
+	if (i / 10 != 0)
 	{
 		print_number(i / 10);
 	}
