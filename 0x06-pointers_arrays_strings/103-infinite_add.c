ed41dceef7fcf7de310232d75789e24e1018c1ac
Sherif Awad <sherif.hero1@gmail.com>
2023-08-24 15:58:54 +0300

    test

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,6 +22,21 @@ char *rev_str(char *n, int leng)
 	return (n);
 }
 
+/**
+ * calc_leng - calclate the string length
+ * @n: is the array
+ * Return: leng
+ */
+
+int calc_leng(char *n)
+{
+	int leng = 0;
+
+	while (*(n++))
+		leng++;
+	return (leng);
+}
+
 
 /**
 * infinite_add - add two number
@@ -36,29 +51,23 @@ char *rev_str(char *n, int leng)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int leng1 = 0, leng2 = 0, car = 0, sum, i = 0;
+	int leng1 = calc_leng(n1), leng2 = calc_leng(n2), car = 0, sum, i = 0, d1, d2;
+	int max_leng = leng1 > leng2 ? leng1 : leng2;
 
-	while (n1[leng1] != '\0')
-		leng1++;
-	while (n2[leng2] != '\0')
-		leng2++;
 	while (leng2 >= 0 || leng1 >= 0 || car)
 	{
-		sum = car;
-		if (leng1 > 0)
-		{
-			sum += n1[leng1 - 1] - '0';
-			leng1--;
-		}
-		if (leng2 > 0)
-		{
-			sum += n2[leng2 - 1] - '0';
-			leng2--;
-		}
+		d1 = leng1 > 0 ? n1[leng1 - 1] - '0' : 0;
+		d2 = leng2 > 0 ? n2[leng2 - 1] - '0' : 0;
+		sum = car + d1 + d2;
+
 		if (i + 1 >= size_r)
 			return (0);
-		r[i++] = sum % 10 + '0';
+		r[i++] = (sum % 10) + '0';
 		car = sum / 10;
+		if (leng1 > 0)
+			leng1--;
+		if (leng2 > 0)
+			leng2--;
 	}
 	r[i] = '\0';
 	return (rev_str(r, i));
