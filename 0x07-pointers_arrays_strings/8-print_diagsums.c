ac631a71a4bbda534e29be69fda9ff0065ad117b
Sherif Awad <sherif.hero1@gmail.com>
2023-08-28 17:36:45 +0300

    diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[(size * i) + i];
-		sum2 += a[size * (i + 1) - (i + 1)];
+		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
