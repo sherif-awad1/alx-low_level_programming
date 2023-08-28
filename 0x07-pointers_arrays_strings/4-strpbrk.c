bb3b84ab7556a6c34b55efb1655d2acf6f724865
Sherif Awad <sherif.hero1@gmail.com>
2023-08-28 16:00:18 +0300

    search

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: the string we will serch in
+ * @accept: the string will compare to
+ * Return: accept or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0;
+
+
+	if (*s == '\0' || *accept == '\0')
+	{
+		return ('\0');
+	}
+
+	for (i = 0;*s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			{
+				if (*s == accept[j])
+				{
+					return (s);
+				}
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
