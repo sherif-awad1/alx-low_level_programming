44282ae945facc8875fb378609b51ebc6d7d8377
Sherif Awad <sherif.hero1@gmail.com>
2023-09-01 18:21:02 +0300

    adjust betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 				if (argv[i][j] < '0' || argv[i][j] > '9')
 				{
 					printf("Error\n");
-                                	return (1);
+					return (1);
 				}
 			}
 			add += atoi(argv[i]);
