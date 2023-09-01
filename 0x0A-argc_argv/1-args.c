27bccc4c529e0491e7db5fba2b955961c68524ac
Sherif Awad <sherif.hero1@gmail.com>
2023-09-01 16:42:11 +0300

    count the variabal

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mian - to print the number of arguments
+ * @argc: count the number of argmeant 
+ * @argv: point to the variabal
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[]__attribute__ ((unused)))
+{
+
+	printf("%d\n", argc);
+	return (0);
+}
