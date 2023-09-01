7c8fbfcffca5a5fd6d3eb40863d53d201812c6b7
Sherif Awad <sherif.hero1@gmail.com>
2023-09-01 14:51:52 +0300

    solbe betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - to print the program name 
- *
+ * main - to print the program name
+ * @argc: the count
+ * @argv: ponter to values
  * Return: Always 0
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
