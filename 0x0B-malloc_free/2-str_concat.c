e0b43ff132d5ca4f15ceb93bd814d040b953ab0f
Sherif Awad <sherif.hero1@gmail.com>
2023-09-05 12:47:18 +0300

    con

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
 
 int _strleng(char *str)
 {
-	int leng;
+	unsigned int leng;
 
 	while (str[leng] != '\0')
 	{
@@ -27,4 +27,32 @@ int _strleng(char *str)
 
 char *str_concat(char *s1, char *s2)
 {
+	unsigned int leng1, leng2, i, j;
+	char *s3:
+	leng1 = _strleng(s1);
+	leng2 = _strleng(s2);
+
+	s3 = malloc((leng1 + leng2 + 1) * sizeof(char));
+
+	if (s1);
+	{
+		while (i < leng1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < (leng1 + leng2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
+}
 
