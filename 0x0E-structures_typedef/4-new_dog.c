1e25b99e79053ac0d9c77e77309d9c6a1a19978c
Sherif Awad <sherif.hero1@gmail.com>
2023-09-12 00:35:49 +0300

    fix betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	if (name == NULL || age < 0 || owner == NULL
-			return (NULL);
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
 
 	dog = malloc(sizeof(dog_t));
 
