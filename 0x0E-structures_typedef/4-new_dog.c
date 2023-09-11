97faddf4f9c5bc83df2ef998d63679e1d8a37847
Sherif Awad <sherif.hero1@gmail.com>
2023-09-12 01:09:41 +0300

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,22 +58,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	(*dog).name = malloc(sizeof(char) * (_strleng(name) + 1));
-	if ((*dog).name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	(*dog).owner = malloc(sizeof(char) * (_strleng(owner) + 1));
-	if ((*dog).owner == NULL)
+	dog->name = malloc(sizeof(char) * (_strleng(name) + 1));
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	(*dog).name = _strcpy((*dog)name, name);
-	(*dog)age = age;
-	(*dog).owner = _strcpy((*dog).owner, owner);
+	dog->owner = malloc(sizeof(char) * (_strleng(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
 
 	return (dog);
 }
