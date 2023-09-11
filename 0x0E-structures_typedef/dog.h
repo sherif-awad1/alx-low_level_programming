bfbc334524f58ec26ee32aa8ed01f546c30d66bc
Sherif Awad <sherif.hero1@gmail.com>
2023-09-11 20:16:14 +0300

    header after fix betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: ownername
+ */
+
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 } dog;
-
-
-#endif /* stract dog */
+#endif /* stract dog - define stract with name age and owner */
