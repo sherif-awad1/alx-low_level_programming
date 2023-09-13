91629e8bfe3520f23d467676ca64c5cef083367c
Sherif Awad <sherif.hero1@gmail.com>
2023-09-13 16:57:07 +0300

    small calc

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,59 +1,60 @@
-#ifndef _CALC_H
-#define _CALC_H
+#include "3-calc.h"
 
 
-
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
  * op_add -  returns the sum of a and b
  * @a: first integer
  * @b: second integer
  * Return: a+b
  */
-int op_add(int a, int b);
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
 /**
  * op_sub -  returns the sub of a and b
  * @a: first integer
  * @b: second integer
  * Return: a - b
  */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 /**
  * op_mul -  returns the product of a and b
  * @a: first integer
  * @b: second integer
  * Return: a * b
  */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 /**
  * op_div -  returns the division of a and b
  * @a: first integer
  * @b: second integer
  * Return: a / b
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
 /**
  * op_mod -  returns the reminder of a and b
  * @a: first integer
  * @b: second integer
  * Return: a % b
  */
-int op_mod(int a, int b);
-
-
-
-
-
-int (*get_op_func(char *s))(int, int);
-#endif
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
