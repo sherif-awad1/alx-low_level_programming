7d25ec3135a588d6731760aa4f05419edf54c7b5
Sherif Awad <sherif.hero1@gmail.com>
2023-09-13 14:24:39 +0300

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 
 int _putchar(char c);
 
@@ -11,6 +12,13 @@ int _putchar(char c);
  */
 void print_name(char *name, void (*f)(char *));
 
+/**
+ * array_iterator - executes a function
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 
