82a4150a9b69ca5e85172b836ba76f8e4976d981
Sherif Awad <sherif.hero1@gmail.com>
2023-09-13 13:51:07 +0300

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints the name
+ * @name: string name
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * _putchar - print a leater
+ * @c: the letter to print
+ * Retrun: print formila
+ */
+int _putchar(char c);
+
+/**
+ * print_name - prints the name
+ * @name: string name
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
