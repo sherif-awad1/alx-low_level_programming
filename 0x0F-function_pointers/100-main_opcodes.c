97618f3cb02da7d2241d1edd57bda2aad3fca7b5
Sherif Awad <sherif.hero1@gmail.com>
2023-09-13 17:34:22 +0300

    test 1

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - to print the opcode
+ * @argc: number of argment
+ * @argv: pointer to array of argment
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	int byt, i;
+	char opcode;
+
+	int (*ptr)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	byt = atoi(argv[1]);
+
+	if (byt < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < byt; i++)
+	{
+		opcode = ptr;
+		printf("%.2x", opcode);
+
+		if (i == byt)
+			continue;
+
+		printf("\t");
+
+		ptr++;
+	}
+	printf("\n");
+
+	return (0);
+}
