a89ebcdb2632bef65021bd90536cdabb5260e5a3
Sherif Awad <sherif.hero1@gmail.com>
2023-09-13 15:22:43 +0300

    serch

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -20,6 +20,19 @@ void print_name(char *name, void (*f)(char *));
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * int_index - searches for an integer
+ * @array: the array of integer
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: -1 in no element match and i if matach
+ */
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+
+
 
 
 
