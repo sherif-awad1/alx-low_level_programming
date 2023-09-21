65cf4880068afca6189c91e4402383309f5c37b0
Sherif Awad <sherif.hero1@gmail.com>
2023-09-21 23:16:23 +0300

    solve berry

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * first - print main
  */
 
-void first(void) __attrobite__ ((constructor))
+void __attribute__((constructor)) first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
