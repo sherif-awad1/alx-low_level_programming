3f30a94f4154bf084b78c6435fdf68cea58c42e7
Sherif Awad <sherif.hero1@gmail.com>
2023-09-21 23:14:16 +0300

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - print main
+ */
+
+void first(void) __attrobite__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
