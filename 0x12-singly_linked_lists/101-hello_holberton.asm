e4b1732eb1f7ae5ad962d59d3f64f70578d60d6d
Sherif Awad <sherif.hero1@gmail.com>
2023-09-21 23:35:51 +0300

    pleas work

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,21 @@
-		global main
-		extern printf
-	main:
-		mov	edi, fmt
-		xor	eax, eax
-		call	printf
-		mov	eax, 0
-		ret
-	fmt: db `\Hello, Holberton`,0
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
