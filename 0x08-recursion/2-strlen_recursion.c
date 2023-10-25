488c1bdf860e7110c3f6a18975059e146241623e
Sherif Awad <sherif.hero1@gmail.com>
2023-10-24 23:52:25 -0700

    strleng

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,3 +2,17 @@
 * File: _putchar.c
 * Author: Sherif Awad
 */
+#include "main.h"
+
+/**
+ * _strlen_recursion - to get the length of string
+ * @s: string to count
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
