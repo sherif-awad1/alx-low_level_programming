402974d2d838cec79d7c35ab8250b9798ef03e8d
Sherif Awad <sherif.hero1@gmail.com>
2023-10-25 00:09:47 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,3 +2,20 @@
 * File: _putchar.c
 * Author: Sherif Awad
 */
+#include "main.h"
+
+/**
+ * _pow_recursion - the get x power of y
+ * @x: integer to get the power of it
+ * @y: the power
+ * Return: -1 if y less than 0 or x power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, (y - 1)));
+}
