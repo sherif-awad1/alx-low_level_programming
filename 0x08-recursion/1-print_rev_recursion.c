ea4e005f693c1a02d87b84c9b3015c595cac3c9b
Sherif Awad <sherif.hero1@gmail.com>
2023-10-24 23:39:31 -0700

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,3 +2,17 @@
 * File: _putchar.c
 * Author: Sherif Awad
 */
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: strint to reverse and print
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
