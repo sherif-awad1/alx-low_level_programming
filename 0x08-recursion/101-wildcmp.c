4b93b5b0ab0dc506a58913ae4472782c3b2e6a15
Sherif Awad <sherif.hero1@gmail.com>
2023-10-25 02:38:07 -0700

    solve

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -52,7 +52,7 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 		return (wildcmp(s1 + 1, *s1 == '*' ? s2 : s2 + 1));
 	}
-	if (!*a1 || !s2)
+	if (!*s1 || !s2)
 		return (0);
 	if (*s2 == '*')
 	{
