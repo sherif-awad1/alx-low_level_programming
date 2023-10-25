492eb9007aebf1ce3988dd385a832849632f6f72
Sherif Awad <sherif.hero1@gmail.com>
2023-10-25 00:00:49 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,3 +2,19 @@
 * File: _putchar.c
 * Author: Sherif Awad
 */
+#include "main.h"
+
+/**
+ * factorial - to fint the factoial if integer
+ * @n: interger to get the fectirial
+ * Return: -1 if 0 of less and 1 if more than 0 els the factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
