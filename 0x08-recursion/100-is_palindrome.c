1c395d11a20857bb42c7e2fba8c5c27371a13efa
Sherif Awad <sherif.hero1@gmail.com>
2023-10-25 01:44:23 -0700

    pal

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,3 +2,42 @@
 * File: _putchar.c
 * Author: Sherif Awad
 */
+#include "main.h"
+
+/**
+ * _strlen_recursion - get the length of the string
+ * @s: string to count
+ * Return: the function
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * pal_helper - to check if the end and start is equal
+ * @s: the string
+ * @str: the start of the string
+ * @end: end of the string
+ * Return: the function
+ */
+int pal_helper(char *s, int str, int end)
+{
+	if (str >= end)
+		return (0);
+	if (s[str] != s[end])
+		return (0);
+	return (pal_helper(s, str + 1, end - 1));
+}
+/**
+ * is_palindrome - to check if the sting is plindrom or not
+ * @s: string to check
+ * Return: pal helper
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (pal_helper(s, 0, _strlen_recurstion(s) - 1));
+}
