eaecffe1c5d5cc8eaa586353d5778455fc60b004
Sherif Awad <sherif.hero1@gmail.com>
2023-10-27 05:37:40 -0700

    mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,35 @@
 /*
- * File: whats my name to print the file name
+ * File: mul to multplay 2 integer
  *
  * Author: Sherif Awad
  */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to multiply two number
+ * @argc: number of argment
+ * @argv: the argmen array
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, mul = 1;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *=atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
