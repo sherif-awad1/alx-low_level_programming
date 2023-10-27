13440b382504b4315d94485a4f45e995e20fa490
Sherif Awad <sherif.hero1@gmail.com>
2023-10-27 08:38:07 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,40 @@
 /*
- * File: whats my name to print the file name
+ * File: add.c add numbe
  *
  * Author: Sherif Awad
  */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to add postive
+ * @argc: number of argm
+ * @argv: argment array
+ * Return: 0 as success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
