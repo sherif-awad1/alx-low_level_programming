fcfccc3c6639d4e814671c5b93892b2d2ad1473d
Sherif Awad <sherif.hero1@gmail.com>
2023-10-30 10:59:58 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,52 @@
 /*
- * File: whats my name to print the file name
+ * File: argstostr concatenates all argumant
  *
  * Author: Sherif Awad
  */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all argument
+ * @ac: number of argment
+ * @av: argment array
+ * Return: NULL or the array
+ */
+char *argstostr(int ac, char **av)
+{
+	char *arg;
+	int i, o, p = 0, count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		o = 0;
+		while (av[i][o])
+		{
+			count++;
+			o++;
+		}
+		count++;
+		i++;
+	}
+	arg = malloc(count + 1 * sizeof(char));
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		o = 0;
+		while (av[i][o])
+		{
+			arg[p] = av[i][o];
+			o++;
+			p++;
+		}
+		arg[p] = '\n';
+		p++;
+		i++;
+	}
+	arg[p] = '\0';
+	return (arg);
+}
