557d7bb16c538a319d31e225f46207b234a2ce16
Sherif Awad <sherif.hero1@gmail.com>
2023-10-30 10:22:57 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,27 @@
 /*
- * File: whats my name to print the file name
+ * File: free grid to free 2d array
  *
  * Author: Sherif Awad
  */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - to free 2d array
+ * @grid: 2d array
+ * @height: the hight of the 2d array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
