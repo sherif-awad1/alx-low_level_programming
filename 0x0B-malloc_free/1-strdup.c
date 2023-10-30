f57d1e582a6d9609353cc70a6f6c78181b5620ad
Sherif Awad <sherif.hero1@gmail.com>
2023-10-30 09:38:25 -0700

    copy string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,37 @@
 /*
- * File: whats my name to print the file name
+ * File:  _strdub copy of string
  *
  * Author: Sherif Awad
  */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - return copy of string
+ * @str: the string
+ * Return: null or string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0, len = 0;
+	char *nstr;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc((len + 1) * sizeof(char));
+
+	if (nstr == NULL)
+		return (NULL);
+
+	while ((nstr[i] = str[i]) != '\0')
+	{
+		i++;
+	}
+	return (nstr);
+}
