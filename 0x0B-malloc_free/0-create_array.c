7f12268318755f737b65d005e11e274543a3f463
Sherif Awad <sherif.hero1@gmail.com>
2023-10-30 07:58:33 -0700

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+/*
+ * File: creat array to creat an array
+ *
+ * Author: Sherif Awad
+ */
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - to creat an array
+ * @size: array size
+ * @c: char
+ * Retuen: 0 if size 0
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *s = malloc(size * sizeof(char));
+
+	if (size == 0 || s == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		*(s + i) = c;
+		i++;
+	}
+	return (s);
+}
