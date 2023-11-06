554eaf678a3d49b6c352873df84c93ef876a3dca
Sherif Awad <sherif.hero1@gmail.com>
2023-11-06 10:11:53 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: the owner name
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
