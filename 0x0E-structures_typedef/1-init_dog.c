b296c730c9c41ef8e7d31310edd823dbce29e118
Sherif Awad <sherif.hero1@gmail.com>
2023-11-06 10:35:16 -0800

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+/*
+ * file: init dog
+ *
+ * Author: Sherif Awad
+ */
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct
+ * @d: pointer to struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: the owner name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
