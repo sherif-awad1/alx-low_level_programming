1e2a1e86984265ec67fe341255a6ad9e7a615a07
Sherif Awad <sherif.hero1@gmail.com>
2023-11-06 11:04:12 -0800

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+/*
+ * FILE: print dog
+ *
+ * Author: Sherif Awad
+ */
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - to print the struct dog
+ * @d: pointer to stuct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", (*d).name);
+	if ((*d).age < 0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %d\n", (*d).age);
+	if ((*d).owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
