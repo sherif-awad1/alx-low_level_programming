fca2c7fbcd57867364ccef61a50ca58456041ed0
Sherif Awad <sherif.hero1@gmail.com>
2023-11-08 14:18:30 -0800

    files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+/*
+ * File: print name
+ *
+ * Author: Sherif Awad
+ */
+#include "function_pointer.h"
+/**
+ * print_name - print the name
+ * @name: string to print
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,29 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
