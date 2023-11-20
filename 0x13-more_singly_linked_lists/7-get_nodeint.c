329108060c50b96ce67469878c8e8133b9f88ebe
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 06:57:11 -0800

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,24 @@
 /*
- * File: 
+ * File: get_nodeint.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t
+ * @head: pointer to head
+ * @index: the index
+ * Return: the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
 
-
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
