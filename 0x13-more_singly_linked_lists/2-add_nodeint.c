4ea2b681a6d59a791a84e63200882b72f2bc19c7
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 05:19:22 -0800

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,27 @@
 /*
- * File: 
+ * File: add_nodeint
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * add_nodeint - add node to head
+ * @head: pointer to header
+ * @n: intger to place in head
+ * Return: address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
 
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+	return (new);
+}
