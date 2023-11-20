6d4e856422ccdbb27ca98051c2a84560933f2097
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 06:49:04 -0800

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,25 @@
 /*
- * File: 
+ * File: pop_listint.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * pop_listint - delete the head
+ * @head: pointer to head
+ * Return: head node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int h;
 
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	h = (*head)->n;
+	*head = (*head)->next;
 
+	free(temp);
+	return (h);
+}
