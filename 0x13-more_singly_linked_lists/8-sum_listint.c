297057946e042006888d1cf7cf0bc369e7f24bd7
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 07:03:53 -0800

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,22 @@
 /*
- * File: 
+ * File: sum_listint.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * sum_listint - calculates the sume
+ * @head: pointer to head
+ * Return: sum of data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
 
-
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
