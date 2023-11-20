ace7ec42140f8149a418003599167c66d39670c7
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 05:46:24 -0800

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,36 @@
 /*
- * File: 
+ * File: add_nodient_end
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * add_nodeint_end - add node at the tail
+ * @head: pointer to the head
+ * @n: integer to be add
+ * Return: address of the new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *last;
 
+	new = malloc(sizeof(listint_t));
 
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
+}
