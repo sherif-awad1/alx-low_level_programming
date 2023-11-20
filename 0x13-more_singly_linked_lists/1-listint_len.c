6e65562d88c59ad05e587f6d778857c9b8327fa3
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 05:08:08 -0800

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,22 @@
 /*
- * File: 
+ * File: listint_len.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * listint_len - count number of elements
+ * @h: pointer to head
+ * Return: count of elementes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
 
-
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
