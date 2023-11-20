08d46d0b442a2e25948466d52910d1011bd91702
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 07:49:38 -0800

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,36 @@
 /*
- * File: 
+ * File: delete_nodeint.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete node
+ * @head: pointer to head
+ * @index: the node we want to delete
+ * Return: 1 on sccess
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *copy = *head;
+	unsigned int nodes;
 
-
+	if (copy == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+	for (nodes = 0; nodes < (index - 1); nodes++)
+	{
+		if (copy->next == NULL)
+			return (-1);
+		copy = copy->next;
+	}
+	temp = copy->next;
+	copy->next = temp->next;
+	free(temp);
+	return (1);
+}
