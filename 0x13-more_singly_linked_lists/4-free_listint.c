8e68bed3a2ba6871c8e1934fcbd2930c4d84c7eb
Sherif Awad <sherif.hero1@gmail.com>
2023-11-20 06:31:58 -0800

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,21 @@
 /*
- * File: 
+ * File: free_listint
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * free_listint - free listint_t
+ * @head: pointer to head
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
 
-
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
