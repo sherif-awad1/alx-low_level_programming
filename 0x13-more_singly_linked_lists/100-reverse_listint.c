89dd172daf183bed581a3d6809e05cd174453470
Sherif Awad <sherif.hero1@gmail.com>
2023-11-21 03:34:27 -0800

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,31 @@
 /*
- * File: 
+ * File: reverse_listint.c
  *
  * Author: Sherif Awad
  */
+#include "lists.h"
+/**
+ * reverse_listint - reverses the list
+ * @head: pointer to head
+ * Return: head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *h, *e;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 
+	e = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		h = (*head)->next;
+		(*head)->next = e;
+		e = *head;
+		*head = h;
+	}
+	(*head)->next = e;
+
+	return (*head);
+}
