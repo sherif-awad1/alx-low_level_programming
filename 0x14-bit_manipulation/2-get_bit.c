01d55eb2f02d9951a7f661154c9d7522e0cddb3d
Sherif Awad <sherif.hero1@gmail.com>
2023-11-23 08:52:35 -0800

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,4 +5,16 @@
  */
 #include "main.h"
 /**
- * get_bit - 
+ * get_bit - the value of a bit at a given index
+ * @n: bit
+ * @index: the index
+ * Return: index value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
+}
