72985928c0b34c3ceadf6cf1a9db5e4e6a51bd55
Sherif Awad <sherif.hero1@gmail.com>
2023-11-27 04:04:43 -0800

    creat task file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ *
+ * Author: Sherif awaw
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* main.h */
