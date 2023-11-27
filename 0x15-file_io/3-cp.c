e7bdb5bd644e60142d1bd9bed0cc8d435528f2fe
Sherif Awad <sherif.hero1@gmail.com>
2023-11-27 06:01:23 -0800

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+/*
+ * File: cp.c
+ *
+ * Author: Sherif Awad
+ */
+#include "main.h"
+#include <stdarg.h>
+/**
+ * buf_alcater - allocat buffer
+ * @filename: pointer to file
+ * Return: buffer
+ */
+char *buf_alcater(char *filename)
+{
+	char *buf;
+
+	buf = malloc(1024);
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (buf);
+}
+/**
+ * file_closer - close file
+ * @file: file
+ */
+void file_closer(int file)
+{
+	int clos;
+
+	clos = close(file);
+	if (clos == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
+/**
+ * main - copy from file to another file
+ * @argc: argmen count
+ * @argv: argments
+ * Return: alway 0 success
+ */
+int main(int argc, char **argv)
+{
+	int frm, to, red, rit;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(98);
+	}
+	buf = buf_alcater(argv[2]);
+	frm = open(argv[1], O_RDONLY);
+	red = read(frm, buf, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0663);
+	do {
+		if (frm == -1 || rit == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+		rit = write(to, buf, red);
+		if (to == -1 || rit == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+		red = read(frm, buf, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (red > 0);
+	free(buf);
+	file_closer(frm);
+	file_closer(to);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content);
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <elf.h>
 
 
 
