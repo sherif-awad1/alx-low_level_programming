b2052b3a25296924c87b7df872bc5d3f5ee54a6e
Sherif Awad <sherif.hero1@gmail.com>
2024-01-15 21:58:41 +0200

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Sherif Awad/alx-low_level_programming/raw/master/0x18-dynamic_libraries/crack.so
+wget -P /tmp https://github.com/sherif-awad1/alx-low_level_programming/raw/c9a3716775fe50d914503bb74152fb888a214c14/0x18-dynamic_libraries/crack.so
 export LD_PRELOAD=/tmp/crack.so
