d5da1a931f3fe612e7feb71bf38a8cacfc3d3062
Sherif Awad <sherif.hero1@gmail.com>
2024-04-03 17:35:59 -0700

    lake

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+function def island_perimeter(grid):
+that returns the perimeter of the island described in grid:
 """
 
 
